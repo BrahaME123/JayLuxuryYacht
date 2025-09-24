45116b44d30aff3be3c3ebb2764d73072137b62e
BrahaME123 <gamermaxjuegos@gmail.com>
2025-09-23 21:21:23 -0600

    First commit

---
+++ slide.js
@@ -0,0 +1,22 @@
+  const slides = document.querySelectorAll(".slide");
+  const dots = document.querySelectorAll(".dot");
+  let index = 0;
+
+  function showSlide(i) {
+    slides.forEach((slide, n) => {
+      slide.classList.toggle("active", n === i);
+      dots[n].classList.toggle("active", n === i);
+    });
+    index = i;
+  }
+
+  function nextSlide() {
+    index = (index + 1) % slides.length;
+    showSlide(index);
+  }
+
+  setInterval(nextSlide, 4000);
+
+  dots.forEach((dot, i) => {
+    dot.addEventListener("click", () => showSlide(i));
+  });
